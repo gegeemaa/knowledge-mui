dd0fdf72030dedb16a184285e745e0d086942de2
gegeemaa <gerelmaaskh@gmail.com>
2021-08-13 13:14:53 +0200

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Paper, Grid } from '@material-ui/core'
 import Editor from 'rich-markdown-editor'
 const text = `
-## Welcome to the "Memorizing note" application!
+## Welcome to the "Note memorizing" application!
 
 This application is dedicated to register notes or anything you are learning and memorize them according to specific rules which are based on brain scientience. Application is inspired by Anki or rules to remember are similar to Ankis rules.
 
@@ -12,7 +12,7 @@ Notes are divided into the following 3 categories:
   2. Learning notes
   3. Reviewing notes
 
-When you click on the "Work on notes" menu, the number of notes which are due today and their categories will be shown. When you continue by clicking on the "Study now" button, the first note's title will be shown by giving you some time to think about the answer. By clicking on the "Show answer" button, a related answer should be shown. Depending on how your answer you have thought matches with the answer, you should grade your note. Grading notes means that you will decide after how much time notes will be shown. Let's call this time "Waiting time."
+When you click on the "Work on notes" menu, the number of notes on which to work today and their categories will be shown. When you continue by clicking on the "Study now" button, the first note's title will be shown by giving you some time to think about the answer. By clicking on the "Show answer" button, a related answer should be shown. Depending on how your answer you have thought matches with the answer, you should grade your note. Grading notes means that you will decide after how much time notes will be shown. Let's call this time "Waiting time."
  
 There are several grading buttons on note and number of buttons and it's related waiting time depends on note's category. 
 
